64ebab812d31642ae2b5975b639b77466822008d
Andrii Honda <andriy.gonda@gmail.com>
2022-05-11 15:54:25 +0300

    add registration form

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import DevicesView from "@/views/DevicesView";
 import MonitoringView from "@/views/MonitoringView";
 import ReportView from "@/views/ReportView";
+import LoginView from "@/views/LoginView";
+import RegisterView from "@/views/RegisterView";
 
 Vue.use(VueRouter)
 
@@ -25,7 +27,12 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/LoginView')
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
   }
 ]
 
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,23 @@
 <template>
-<p>register</p>
+<div class="container">
+  <div class="logo-container">
+    <img src="../assets/logo.png" alt="TelematiX" class="logo-image">
+  </div>
+  <h1 class="text text-danger">Registration</h1>
+  <pre class="text text-secondary">Enter user data for registration</pre>
+  <div class="registration-form container">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username">
+
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email">
+
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password">
+
+    <button class="btn btn-danger">Register now</button>
+  </div>
+</div>
 </template>
 
 <script>
@@ -9,5 +27,18 @@ export default {
 </script>
 
 <style scoped>
+.logo-image {
+  padding-bottom: 50px;
+}
 
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn {
+  margin-top: 15px;
+  width: 100%;
+}
 </style>
